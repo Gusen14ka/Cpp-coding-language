adc9881bc3406deced11ef017979982a91b2cf74
Gusen14ka <faktur2005@yandex.ru>
2025-02-15 13:03:27 +0300

    Fixed ReadEquation function

--- Lab_1/Lab_1.cpp
+++ Lab_1/Lab_1.cpp
@@ -31,6 +31,7 @@ bool ReadEquation(array<double, NUM_OF_COEF>& equation) {
     regex format_full_eq(R"(^\s*([+-]?\d*(?:\.\d*)?)\s*x\^2\s*([+-]\d*(?:\.\d*)?)\s*x\s*([+-]\d*(?:\.\d*)?)\s*=\s*0\s*$)");
     regex format_without_b(R"(^\s*([+-]?\d*(?:\.\d*)?)\s*x\^2\s*([+-]\d*(?:\.\d*)?)\s*=\s*0\s*$)");
     regex format_without_c(R"(^\s*([+-]?\d*(?:\.\d*)?)\s*x\^2\s*([+-]\d*(?:\.\d*)?)\s*x\s*=\s*0\s*$)");
+    regex format_without_b_and_c(R"(^\s*([+-]?\d*(?:\.\d*)?)\s*x\^2\s*=\s*0\s*$)");
     smatch match;
 
     if (regex_match(input, match, format_full_eq)) {
@@ -41,7 +42,7 @@ bool ReadEquation(array<double, NUM_OF_COEF>& equation) {
         //coef c
         equation[2] = get_coefficient(match[3].str());
     }
-    else if((regex_match(input, match, format_without_b))){
+    else if(regex_match(input, match, format_without_b)){
         //coef a
         equation[0] = get_coefficient(match[1].str());
         //coef b
@@ -49,11 +50,19 @@ bool ReadEquation(array<double, NUM_OF_COEF>& equation) {
         //coef c
         equation[2] = get_coefficient(match[2].str());
     }
-    else if ((regex_match(input, match, format_without_c))) {
+    else if (regex_match(input, match, format_without_c)) {
         //coef a
         equation[0] = get_coefficient(match[1].str());
         //coef b
         equation[1] = get_coefficient(match[2].str());
+        //coef c
+        equation[2] = 0.0;
+    }
+    else if (regex_match(input, match, format_without_b_and_c)) {
+        //coef a
+        equation[0] = get_coefficient(match[1].str());
+        //coef b
+        equation[1] = 0.0;
         //coef c
         equation[2] = 0.0;
     }
