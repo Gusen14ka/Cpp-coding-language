e3f39afedd5ee5ab3fcfabfb6d6cbeb6a44ed180
Gusen14ka <faktur2005@yandex.ru>
2025-03-27 23:32:12 +0300

    Create Student class and WriteAnswer function

--- Lab_2/Lab_2.cpp
+++ Lab_2/Lab_2.cpp
@@ -1,134 +1,194 @@
 #include <array>
-#include <iostream>
-#include <regex>
 #include <fstream>
+#include <iostream>
 #include <optional>
+#include <regex>
+#include <iomanip>
+#include <sstream>
 
+struct Solution
+{
+    int numRoots = NAN;
+    double root1 = NAN;
+    double root2 = NAN;
+};
 
+struct EquationCoefs
+{
+    double a = NAN;
+    double b = NAN;
+    double c = NAN;
+};
 
-class Equation {
+class Equation
+{
 
-    public:
-        struct Solution {
-            int numRoots = NAN;
-            double root1 = NAN;
-            double root2 = NAN;
-        };
-
-        struct EquationCoefs {
-            double a = NAN;
-            double b = NAN;
-            double c = NAN;
-        };
-
+  private:
     EquationCoefs equation;
     Solution solution;
 
-    private:
-        double get_coefficient(const std::string& str) {
-            if (str.empty() || str == "+") {
-                return 1.0;
-            }
-            if (str == "-") {
-                return -1.0;
-            }
-            return stod(str);
+    double get_coefficient(const std::string &str)
+    {
+        if (str.empty() || str == "+")
+        {
+            return 1.0;
         }
-
-        int CountNumberOfNaturalDigits(double n) {
-            int digits = 1;
-            while (n > 10) {
-                n /= 10.0;
-                digits++;
-            }
-            return digits;
+        if (str == "-")
+        {
+            return -1.0;
         }
+        return stod(str);
+    }
 
-        bool RealEqualityDouble(double a, double b) {
-            constexpr double epsilon = 1e-9;
-            return std::abs(a - b) < epsilon;
+    int CountNumberOfNaturalDigits(double n)
+    {
+        int digits = 1;
+        while (n > 10)
+        {
+            n /= 10.0;
+            digits++;
         }
+        return digits;
+    }
 
-    public:
-        void ReadEquation(std::ifstream& file) {
-            std::string input;
-            getline(file, input);
-            if (input.empty()) {
-                throw std::runtime_error("Empty file");
-                return;
-            }
+    bool RealEqualityDouble(double a, double b)
+    {
+        constexpr double epsilon = 1e-9;
+        return std::abs(a - b) < epsilon;
+    }
 
-            std::regex format_full_eq(R"(^\s*([+-]?\d*(?:\.\d*)?)\s*x\^2\s*([+-]\d*(?:\.\d*)?)\s*x\s*([+-]\d*(?:\.\d*)?)\s*=\s*0\s*$)");
-            std::regex format_without_b(R"(^\s*([+-]?\d*(?:\.\d*)?)\s*x\^2\s*([+-]\d*(?:\.\d*)?)\s*=\s*0\s*$)");
-            std::regex format_without_c(R"(^\s*([+-]?\d*(?:\.\d*)?)\s*x\^2\s*([+-]\d*(?:\.\d*)?)\s*x\s*=\s*0\s*$)");
-            std::regex format_without_b_and_c(R"(^\s*([+-]?\d*(?:\.\d*)?)\s*x\^2\s*=\s*0\s*$)");
-            std::smatch match;
+  public:
+    const EquationCoefs &getEquation() const
+    {
+        return equation;
+    }
 
-            if (regex_match(input, match, format_full_eq)) {
-                //coef a
-                equation.a = get_coefficient(match[1].str());
-                //coef b
-                equation.b = get_coefficient(match[2].str());
-                //coef c
-                equation.c = get_coefficient(match[3].str());
-            }
-            else if(regex_match(input, match, format_without_b)){
-                //coef a
-                equation.a = get_coefficient(match[1].str());
-                //coef b
-                equation.b = 0.0;
-                //coef c
-                equation.c = get_coefficient(match[2].str());
-            }
-            else if (regex_match(input, match, format_without_c)) {
-                //coef a
-                equation.a = get_coefficient(match[1].str());
-                //coef b
-                equation.b = get_coefficient(match[2].str());
-                //coef c
-                equation.c = 0.0;
-            }
-            else if (regex_match(input, match, format_without_b_and_c)) {
-                //coef a
-                equation.a = get_coefficient(match[1].str());
-                //coef b
-                equation.b = 0.0;
-                //coef c
-                equation.c = 0.0;
-            }
-            else {
-                throw std::runtime_error("The entered equation does not satisfy the format ax^2+bx+c=0");
-                return;
-            }
+    const Solution &getSolution() const
+    {
+        return solution;
+    }
+
+    void ReadEquation(std::ifstream &file)
+    {
+        std::string input;
+        getline(file, input);
+        if (input.empty())
+        {
+            throw std::runtime_error("Empty file");
             return;
         }
 
-        void SolveEquation() {
-            if (std::isnan(equation.a) || std::isnan(equation.b) || std::isnan(equation.c)) {
-                throw std::runtime_error("Empty equation");
-                return;
-            }
-            double discriminant = pow(equation.b,2) - 4 * equation.a * equation.c;
-            if (discriminant < 0) {
-                solution.numRoots = 0;
-                return;
-            }
-            double sqrt_of_discrim = std::sqrt(discriminant);
-            double root1 = ((-1.0) * equation.b + sqrt_of_discrim) / (2 * equation.a);
-            double root2 = ((-1.0) * equation.b - sqrt_of_discrim) / (2 * equation.a);
-            // Checking for a root match
-            if (RealEqualityDouble(root1, root2)) {
-                solution.numRoots = 1;
-                solution.root1 = root1;
-                solution.root2 = root1;
-                return;
-            }
-            else {
-                solution.numRoots = 2;
-                solution.root1 = std::min(root1, root2);
-                solution.root2 = std::max(root1, root2);
-                return;
-            }
-        }
+        std::regex format_full_eq(
+            R"(^\s*([+-]?\d*(?:\.\d*)?)\s*x\^2\s*([+-]\d*(?:\.\d*)?)\s*x\s*([+-]\d*(?:\.\d*)?)\s*=\s*0\s*$)");
+        std::regex format_without_b(R"(^\s*([+-]?\d*(?:\.\d*)?)\s*x\^2\s*([+-]\d*(?:\.\d*)?)\s*=\s*0\s*$)");
+        std::regex format_without_c(R"(^\s*([+-]?\d*(?:\.\d*)?)\s*x\^2\s*([+-]\d*(?:\.\d*)?)\s*x\s*=\s*0\s*$)");
+        std::regex format_without_b_and_c(R"(^\s*([+-]?\d*(?:\.\d*)?)\s*x\^2\s*=\s*0\s*$)");
+        std::smatch match;
 
+        if (regex_match(input, match, format_full_eq))
+        {
+            // coef a
+            equation.a = get_coefficient(match[1].str());
+            // coef b
+            equation.b = get_coefficient(match[2].str());
+            // coef c
+            equation.c = get_coefficient(match[3].str());
+        }
+        else if (regex_match(input, match, format_without_b))
+        {
+            // coef a
+            equation.a = get_coefficient(match[1].str());
+            // coef b
+            equation.b = 0.0;
+            // coef c
+            equation.c = get_coefficient(match[2].str());
+        }
+        else if (regex_match(input, match, format_without_c))
+        {
+            // coef a
+            equation.a = get_coefficient(match[1].str());
+            // coef b
+            equation.b = get_coefficient(match[2].str());
+            // coef c
+            equation.c = 0.0;
+        }
+        else if (regex_match(input, match, format_without_b_and_c))
+        {
+            // coef a
+            equation.a = get_coefficient(match[1].str());
+            // coef b
+            equation.b = 0.0;
+            // coef c
+            equation.c = 0.0;
+        }
+        else
+        {
+            throw std::runtime_error("The entered equation does not satisfy the format ax^2+bx+c=0");
+            return;
+        }
+        return;
+    }
+
+    void SolveEquation()
+    {
+        if (std::isnan(equation.a) || std::isnan(equation.b) || std::isnan(equation.c))
+        {
+            throw std::runtime_error("Empty equation");
+            return;
+        }
+        double discriminant = pow(equation.b, 2) - 4 * equation.a * equation.c;
+        if (discriminant < 0)
+        {
+            solution.numRoots = 0;
+            return;
+        }
+        double sqrt_of_discrim = std::sqrt(discriminant);
+        double root1 = ((-1.0) * equation.b + sqrt_of_discrim) / (2 * equation.a);
+        double root2 = ((-1.0) * equation.b - sqrt_of_discrim) / (2 * equation.a);
+        // Checking for a root match
+        if (RealEqualityDouble(root1, root2))
+        {
+            solution.numRoots = 1;
+            solution.root1 = root1;
+            solution.root2 = root1;
+            return;
+        }
+        else
+        {
+            solution.numRoots = 2;
+            solution.root1 = std::min(root1, root2);
+            solution.root2 = std::max(root1, root2);
+            return;
+        }
+    }
 };
+
+class Student
+{
+    std::string Name;
+
+  public:
+    void WriteAnswer(std::ofstream &file, Equation& equation)
+    {
+        std::ostringstream oss;
+        EquationCoefs coefs = equation.getEquation();
+        Solution solution = equation.getSolution();
+        oss << std::fixed << std::setprecision(9) << coefs.a << "x^2";
+        if (coefs.b >= 0) {
+            oss << "+";
+        }
+        oss << std::fixed << std::setprecision(9) << coefs.b << "x";
+        if (coefs.c >= 0) {
+            oss << "+";
+        }
+        oss << std::fixed << std::setprecision(9) << coefs.c;
+        std::string equationStr = oss.str();
+        file << Name << "|" << equationStr << "|" << solution.numRoots << "|"
+             << solution.root1 << "|" << solution.root2 << std::endl;
+    }
+};
+
+int main()
+{
+    Equation student;
+}
